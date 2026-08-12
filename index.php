cfb97bae819426381cb85c9a59815eead6b07ddc
Pablo <pablogahh@gmnail.com>
2026-08-11 23:09:53 -0300

    First commit: Projeto Dessa_Lens

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="shortcut icon" type="image-x/png" href="camera.png" />
+    </head>
+<body>
+
+    <section class="main">
+        <div class="layer"></div><!--layer-->
+        <div class="center">
+            <header>
+            <div class="icons-social">
+                <a href="https://www.whatsapp.com/catalog/553888356130/?app_absent=0" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/andressa_lens/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            </div><!--icons-social-->
+
+                 <div class="clear"></div><!--clear-->
+                 
+                <div class="logo">
+                    DESSA_LENS
+                </div><!--logo-->
+                <nav class="desktop">
+                    <ul>
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="index.html" target="_blank">Sobre</a></li>
+                        <li><a href="https://www.whatsapp.com/catalog/553888356130/?app_absent=0" target="_blank">Contato</a></li>
+                    </ul>
+                </nav><!--desktop-->
+
+                <nav class="mobile">
+                    <i class="fa fa-bars"></i>
+                    <ul>
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="index.html" target="_blank">Sobre</a></li>
+                        <li><a href="https://www.whatsapp.com/catalog/553888356130/?app_absent=0" target="_blank">Contato</a></li>
+                    </ul>
+                </nav><!--mobile-->
+                 <div class="clear"></div><!--clear-->
+            </header>
+            <div class="conteudo-header">
+                <h2>Bem vindo(a) ao nosso <span>"Eternizando Momentos Especiais" 🤍✨</span></h2>
+                <p>Seja bem-vindo ao mundo das imagens que contam histórias</p>
+                <p> Onde cada clique eterniza momentos únicos. Explore a magia da fotografia conosco!</p>
+                <a href="https://www.whatsapp.com/catalog/553888356130/?app_absent=0" target="_blank">Orçamentos!</a>
+            </div><!--conteudo-header-->
+        </div><!--center-->
+    </section><!--main-->
+
+    <section class="planos">
+        <div class="chamada-planos">
+            <div class="center">
+                <h2>Lista de Preços</h2>
+            </div><!--center-->
+        </div><!--chamada-planos-->
+        <section class="box-planos">
+            <div class="center">
+                <div class="box-planos-single">
+                    <div class="box-planos-wraper">
+                        <h2>Fotográfia Retrato</h2>
+                        <div class="pontos-plano">
+                        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Retrato indiduais;</p>
+                        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Retratos de família;</p>
+                        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Retratos de idosos.</p>
+                        </div><!--pontos-plano-->
+                        <div class="preco">
+                            <h2>A partir de R$100,00</h2>
+                        </div><!--preco-->
+                    </div><!--box-planos-wraper-->
+                </div><!--box-planos-single-->
+
+                <div class="box-planos-single">
+                    <div class="box-planos-wraper">
+                        <h2>Fotográfia Comercial</h2>
+                        <div class="pontos-plano">
+                        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Fotográfia de produtos;</p>
+                        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Eventos corporativos e conferência;</p>
+                        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Imobiliário;</p>
+                        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Fotográfia de alimentos.</p>
+                        </div><!--pontos-plano-->
+                        <div class="preco">
+                            <h2>A partir de R$150,00</h2>
+                        </div><!--preco-->
+                    </div><!--box-planos-wraper-->
+                </div><!--box-planos-single-->
+
+                <div class="box-planos-single">
+                    <div class="box-planos-wraper">
+                        <h2>Fotográfia Editorial</h2>
+                        <div class="pontos-plano">
+                        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Fotográfia de moda;
+                        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Fotos editorias para revistas;</p>
+                        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> Fotos editorias para publicações.</p>
+                        </div><!--pontos-plano-->
+                        <div class="preco">
+                            <h2>A partir de R$120,00</h2>
+                        </div><!--preco-->
+                    </div><!--box-planos-wraper-->
+                </div><!--box-planos-single-->
+
+            </div><!--center-->
+        </section><!--box-planos-->
+    </section><!--planos-->
+
+    <section class="parallax">
+        <div class="overlay-parallax"></div><!--overlay-parallax-->
+            <div class="center">
+                <h2>Sobre Nossos <span style="color: #FFD230;">Serviços</span></h2>
+                <p>Com habilidade e sensibilidade, nosso principal foco é capturar momentos únicos, transformando instantes em memórias eternas através da magia da fotografia.</p>
+                <p>Um ensaio de fotos é a oportunidade de capturar sua essência, contar sua história e celebrar quem você é, em imagens que serão tesouros para toda a vida.</p>
+            </div><!--center-->
+    </section><!--parallax-->
+
+    <section class="final-section">
+        <div class="center">
+            <div class="w50 contato">
+            <h2><div class="borda-efeito"></div>Entre em contato!</h2>
+             <form method="post" action="form.php">
+                <input required type="hidden" name="nome do formulário" value="Formulário 1">
+                <input required type="text" name="nome" placeholder="Insira seu nome...">
+                <input required type="email" name="email" placeholder="Seu e-mail...">
+                <input required  type="text" name="telefone" placeholder="Telefone...">
+                <textarea required name="mensagem" placeholder="Sua mensagem..."></textarea>
+                 <div class="enviar">
+                 <input type="submit" value="Enviar">
+                 </div><!--enviar-->
+             </form>
+            </div><!--w50-contato-->
+
+        <div class="w50 time">
+            <h2><div class="borda-efeito"></div>Quem sou eu?</h2>
+                <div class="w50 equipe-single">
+                    <div class="topo-box-usuario">
+                        <div class="box-avatar"></div>
+                        <div class="info-usuario">
+                            <p style="font-weight: bold;">Andressa Neves</p>
+                            <p style="font-weight: italic;">Fotográfa / Marketing</p>
+                        </div>
+                    </div><!--topo-box-usuario-->
+                
+                    <div class="descricao-usuario-box">
+                        <p>Olá! Tudo bem? </p>
+                        <p>Espero que esteja tudo bem! Estou entrando em contato para compartilhar um pouco do meu trabalho de fotografia, capturando momentos especiais e transformando-os em memórias eternas. Se você ou alguém que conhece está buscando registrar momentos únicos, estou à disposição para oferecer meus serviços. Se não for o caso, adoraria pedir um favor: se puder, compartilhe meu trabalho com seus amigos e familiares. Sua ajuda é muito valiosa para mim! 😊📸</p>
+                    </div><!--descricao-usuario-box-->
+                </div><!--equipe-single-->
+        </div><!--w50-time-->
+
+     <div class="clear"></div>
+
+        </div><!--center-->
+    </section><!--final-section-->
+
+
+<footer>
+		<p>Todos os direitos reservados</p>
+        <p>© Dessa_Lens 2023</p>
+</footer>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.ajaxform.js"></script>
+    <script>
+	$(function(){
+
+$('form').submit(function(){
+
+	if(formularioPreenchido()){
+			var form = $('form');
+			$('form').ajaxSubmit({
+				beforeSubmit:function(){
+					form.find('input[type=submit]').attr('disabled','true');
+					form.find('input[type=submit]').animate({'opacity':'0.4'})
+					form.find('input[type=submit]').attr('value','Carregando');
+
+				},
+				success:function(data){
+					//Aqui você pode inserir uma div, por exemplo.
+					//Qualquer mensagem de sucesso, após o formulario ter sido enviado.
+					alert('Formulário foi enviado com sucesso!');
+					form.find('input[type=submit]').removeAttr('disabled');
+					form.find('input[type=submit]').animate({'opacity':'1'})
+					form.find('input[type=submit]').attr('value','Enviar');
+					form[0].reset();
+				}
+			});
+	}else{
+		alert("Campos Vázios não são permitidos!");
+	}
+
+
+
+	return false;
+})
+
+function formularioPreenchido(){
+	var nome = $('[name=nome]').val();
+	var email = $('[name=email]').val();
+	var telefone = $('[name=telefone]').val();
+	var mensagem = $('[name=mensagem]').val();
+	if(nome === '' || email === '' || telefone === '' || mensagem === ''){
+		return false;
+	}else{
+		return true;
+	}
+}
+
+
+})
+</script>
+
+   <script>
+        $(function(){
+            $('nav.mobile i').click(function(){
+                var el = $(this).parent().find('ul');
+                if(el.is(':visible') == false){
+                 el.fadeIn();
+                }else{
+                    el.fadeOut();
+                }
+            })
+        })
+        </script>
+    <script src="js/functions.js"></script>
+    
+
+</body>
+</html>
---
+++ js/functions.js
@@ -0,0 +1,4 @@
+$(window).on('load',function(){
+    alert("Seja bem vindo")
+})
+
